1a772996269b2024f17fee6d358d83ac47363b6f
沈妍 <maggie@shenyandeMacBook-Pro.local>
2018-04-28 15:50:16 +0800

    add

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Layout } from 'antd';
+
+class Home extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {}
+    }
+
+    componentDidMount () {
+
+    }
+
+    render () {
+       return (
+           <Layout>
+               111
+           </Layout>
+       )
+    }
+}
