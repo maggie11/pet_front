9762a9ee84a62803eea8a11ead5d5954ce9f91fc
沈妍 <maggie@shenyandeMacBook-Pro.local>
2018-04-28 16:07:55 +0800

    del

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-        require('autoprefixer')({
-        	browsers:'>0.1%'
-        }),
-        require('cssnano')({zindex: false, reduceIdents: false})
-    ]
-}
--- src/pages/Main/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './Main.js'
-import './Main.css'
